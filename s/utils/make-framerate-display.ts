dd81d9916456b449032237cf33c3dc4459efd5d6
Paul_Aroo <timmypaul2006@gmail.com>
2022-11-17 21:07:17 +0100

    wip: framerate display

---
+++ s/utils/make-framerate-display.ts
@@ -0,0 +1,18 @@
+
+export function makeFramerateDisplay({getFramerate}: {
+		getFramerate: () => number
+	}) {
+
+	const element = document.createElement("div")
+	element.className = "framerate"
+	element.textContent = "---"
+
+	setInterval(
+		() => element.innerHTML = getFramerate()
+			.toFixed(0)
+			.padStart(3, "X")
+			.replaceAll("X", "&nbsp;"),
+		100
+	)
+	return element
+}
