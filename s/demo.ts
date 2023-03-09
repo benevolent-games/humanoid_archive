13541e16d2503897c57da158fc03aeb51b3e9171
Chase Moskal <chasemoskal@gmail.com>
2023-03-09 13:47:10 -0800

    add: special effects

--- s/demo.ts
+++ s/demo.ts
@@ -9,6 +9,11 @@ import "@babylonjs/core/Meshes/instancedMesh.js"
 import "@babylonjs/loaders/glTF/2.0/index.js"
 import "@babylonjs/core/Culling/ray.js"
 
+import "@babylonjs/core/PostProcesses/index.js"
+import "@babylonjs/core/Rendering/index.js"
+
+import {SSAO2RenderingPipeline} from "@babylonjs/core/PostProcesses/RenderPipeline/Pipelines/ssao2RenderingPipeline.js"
+
 import {makeRealmEcs} from "./realm/ecs.js"
 import {spawnCube} from "./utils/spawn-cube.js"
 import {showCoolGlb} from "./utils/show-cool-glb.js"
@@ -20,6 +25,9 @@ import {Color3, Color4} from "@babylonjs/core/Maths/math.color.js"
 import {SceneLoader} from "@babylonjs/core/Loading/sceneLoader.js"
 import {CascadedShadowGenerator} from "@babylonjs/core/Lights/Shadows/index.js"
 import {Mesh} from "@babylonjs/core/Meshes/mesh.js"
+import {DefaultRenderingPipeline, DepthOfFieldEffectBlurLevel, TonemappingOperator} from "@babylonjs/core/PostProcesses/index.js"
+import {DirectionalLight} from "@babylonjs/core/Lights/directionalLight.js"
+import {HemisphericLight} from "@babylonjs/core/Lights/hemisphericLight.js"
 
 void async function main() {
 	document.querySelector("[data-loading]")!.remove()
@@ -71,7 +79,7 @@ void async function main() {
 		engine,
 		nubContext,
 		renderLoop,
-		walk: 0.7,
+		walk: 0.5,
 		lookSensitivity: {
 			stick: 1/50,
 			mouse: 1/1_000,
@@ -79,11 +87,61 @@ void async function main() {
 	})
 
 	{
+		const direction = new Vector3(0.8, 0.6, -0.9)
+		const backlight = new HemisphericLight("backlight", direction, scene)
+		backlight.intensity = 1
+		backlight.diffuse = new Color3(1, 1, 1)
+	}
+
+	{
 		shadow_generator.autoCalcDepthBounds = true
 		shadow_generator.bias = 0.01
 		shadow_generator.normalBias = 0
 		camera.minZ = 1
 		camera.maxZ = 500
+	}
+
+	{
+		const pipeline = new DefaultRenderingPipeline("default", true, scene, [camera])
+
+		pipeline.fxaaEnabled = true
+		pipeline.samples = 8
+		
+		pipeline.bloomEnabled = true
+		pipeline.bloomThreshold = 0.8;
+		pipeline.bloomWeight = 0.3;
+		pipeline.bloomKernel = 256;
+		pipeline.bloomScale = 0.5;
+
+		pipeline.depthOfFieldEnabled = true
+		pipeline.depthOfFieldBlurLevel = DepthOfFieldEffectBlurLevel.Low
+		pipeline.depthOfField.focusDistance = 100 * 1000
+		pipeline.depthOfField.focalLength = 50
+		pipeline.depthOfField.fStop = 1.4
+
+		pipeline.imageProcessingEnabled = true
+		pipeline.imageProcessing.vignetteEnabled = true
+		pipeline.imageProcessing.vignetteWeight = 0.75
+
+		pipeline.imageProcessing.toneMappingEnabled = true
+		pipeline.imageProcessing.toneMappingType = TonemappingOperator.Photographic
+
+		// pipeline.grainEnabled = true
+		// pipeline.grain.intensity = 5
+		// pipeline.grain.animated = true
+	}
+
+	{
+		const ao_settings = 0.75
+		// const ao_settings = {ssaoRatio: 0.75, blurRatio: 0.75, combineRatio: 1}
+		const ao = new SSAO2RenderingPipeline("ssao", scene, ao_settings, [camera])
+		ao.radius = 10
+		ao.totalStrength = 1.5
+		ao.base = 0.15
+		ao.samples = 16
+		ao.maxZ = 600
+		ao.minZAspect = 0.5
+		// SSAOPipeline.expensiveBlur = true;
 	}
 
 	// const box = await spawnCube(
