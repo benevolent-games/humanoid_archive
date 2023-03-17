a8381d8849110291dc846fe7cee2658590cfaa87
Chase Moskal <chasemoskal@gmail.com>
2023-03-17 14:01:05 -0700

    refine: ecs concept

--- s/ecstacy/concept.ts
+++ s/ecstacy/concept.ts
@@ -1,28 +1,37 @@
 
-// let v3: any
-// let move_vector_from_user_input: any
-// function behaviors(b: any) {}
+// todo: add an example that uses 'local', which
+// is non-networked entity-specific state.
+// like babylon meshes and stuff.
 
-// behaviors(behavior => [
+behaviors(behavior => [
 
-// 	behavior("example counter increment each second")
-// 		.selector(["count"])
-// 		.activity(({meta, state}) => {
-// 			if (meta.host && meta.frequency(1000))
-// 				state.count += 1
-// 		}),
+	behavior("counter increments each second")
+		.selector(["count"])
+		.lifecycle({
+			created: () => {
+				// entity is created
+			},
+			components_changed: ({added, deleted}) => {
+				// handle components that have been added or deleted
+			},
+			activity: ({host, frequency, state}) => {
+				if (host && frequency(1000))
+					state.count += 1
+			},
+			deleted: () => {
+				// entity is deleted
+			},
+		}),
 
-// 	behavior("player can move")
-// 		.selector(["character", "position", "owner"])
-// 		.activity(({id, meta, state, local}) => {
-
-// 			if (meta.host)
-// 				for (const vector of meta.messages_from_client)
-// 					state.position = v3.add(state.position, vector)
-
-// 			else if (meta.frequency(50) && state.owner === id) {
-// 				const vector = move_vector_from_user_input(local.nub_context)
-// 				meta.send_message_to_host(vector)
-// 			}
-// 		}),
-// ])
+	behavior("users can move objects")
+		.selector(["position", "movable", "owner"])
+		.activity(({frequency, user_id, client, host, state, context}) => {
+			if (frequency(50) && state.owner === user_id) {
+				const vector = move_vector_from_user_input(context.nub_context)
+				client.send_to_host(vector)
+			}
+			if (host)
+				for (const vector of host.inbox)
+					state.position = v3.add(state.position, vector)
+		}),
+])
