049f1fa7e1ee1dcb7d1177fc80fd3a65eef301b3
Chase Moskal <chasemoskal@gmail.com>
2023-03-22 13:01:24 -0700

    tweak: toggle camera view syntax and technique

--- s/utils/toggle-camera-view.ts
+++ s/utils/toggle-camera-view.ts
@@ -1,22 +1,29 @@
-import {NubCauseEvent, NubDetail} from "@benev/nubs"
 
+import {NubCauseEvent} from "@benev/nubs"
 import {Scene} from "@babylonjs/core/scene.js"
 import {TargetCamera} from "@babylonjs/core/Cameras/targetCamera.js"
 
-export function toggleCameraView(
-		{scene, first_person_camera, third_person_camera
+export function toggleCameraView({
+		scene, first_person_camera, third_person_camera,
 	}: {
 		scene: Scene
 		first_person_camera: TargetCamera
 		third_person_camera: TargetCamera
 	}) {
 
-	NubCauseEvent.target(window)
+	NubCauseEvent
+		.target(window)
 		.listen(({detail}) => {
-			const toggleKeyPressed = detail.cause === "KeyX" && (detail as NubDetail.Key).pressed
-			if (toggleKeyPressed)
-				if (scene.activeCamera!.id === "first-cam")
-					scene.activeCamera = third_person_camera
-					else scene.activeCamera = first_person_camera
+
+			const toggle_key_is_pressed = (
+				detail.kind === "key" &&
+				detail.cause === "KeyX" &&
+				detail.pressed
+			)
+
+			if (toggle_key_is_pressed)
+				scene.activeCamera = scene.activeCamera === first_person_camera
+					? third_person_camera
+					: first_person_camera
 	})
 }
