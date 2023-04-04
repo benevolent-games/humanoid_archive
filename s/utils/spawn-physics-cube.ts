3fccd656dbe58c60768e184a5f66c8f1a4bd2423
Paul_Aroo <timmypaul2006@gmail.com>
2023-04-04 19:26:23 +0100

    fix: spawn cubes closer to the surface

--- s/utils/spawn-physics-cube.ts
+++ s/utils/spawn-physics-cube.ts
@@ -22,11 +22,23 @@ export async function spawnPhysicsCube(
 		restitution: 0.5,
 	})
 
+	
+	const six_inches = 0.1524
+	const square_root_of_3 = 1.732
+	const cube_width = box.scaling.x * box.getBoundingInfo().maximum.x
+	const max_size_across_cube_diagonally = cube_width * square_root_of_3
+	const distance_from_surface = six_inches + (max_size_across_cube_diagonally / 2)
+
 	const {x, y, z} = position
 	const newPosition = new Vector3(
-		normal.x < 0 ? x - 6 : normal.x > 0 ? x + 6 : x,
-		normal.y < 0 ? y - 6 : normal.y > 0 ? y + 6 : y,
-		normal.z < 0 ? z - 6 : normal.z > 0 ? z + 6 : z
+		normal.x < 0 ? x - distance_from_surface :
+		normal.x > 0 ? x + distance_from_surface : x,
+		
+		normal.y < 0 ? y - distance_from_surface :
+		normal.y > 0 ? y + distance_from_surface : y,
+		
+		normal.z < 0 ? z - distance_from_surface :
+		normal.z > 0 ? z + distance_from_surface : z
 	)
 
 	box.position = newPosition
