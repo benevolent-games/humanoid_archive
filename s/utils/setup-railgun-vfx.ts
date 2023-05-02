9d771b45ff326052bd673438de1e026ae90c6c49
zenkyu <przemekgg2002@gmail.com>
2023-05-03 00:00:14 +0200

    wip: railgun

---
+++ s/utils/setup-railgun-vfx.ts
@@ -0,0 +1,39 @@
+import {StandardMaterial} from "@babylonjs/core/Materials/standardMaterial.js"
+import {Texture} from "@babylonjs/core/Materials/Textures/texture.js"
+import {Vector3} from "@babylonjs/core/Maths/math.js"
+import {Mesh} from "@babylonjs/core/Meshes/mesh.js"
+import {VertexData} from "@babylonjs/core/Meshes/mesh.vertexData.js"
+import {MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder.js"
+import {Scene} from "@babylonjs/core/scene.js"
+import {makeRayMesh} from "./make-ray-mesh.js"
+
+export function setupRaligunVFX(scene: Scene) {
+	var orbTexture = new Texture('https://i.imgur.com/MiISieq.png', scene)
+	var orbMesh = MeshBuilder.CreatePlane('orb', {size: 1}, scene)
+	var orbMat = new StandardMaterial('orbMat', scene)
+	orbMat.disableLighting = true
+	orbMat.emissiveTexture = orbTexture
+	orbMat.opacityTexture = orbTexture
+	orbMesh.material = orbMat
+	orbMesh.scaling.scaleInPlace(1.2)
+
+	var sparkMesh = MeshBuilder.CreatePlane('orb', {size: 1, sideOrientation: VertexData.DOUBLESIDE}, scene)
+	var sparkMat = new StandardMaterial("sparkMat", scene)
+	sparkMat.disableLighting = true
+	sparkMesh.material = sparkMat
+
+	sparkMesh.position.x = -0.5
+	sparkMesh.bakeCurrentTransformIntoVertices()
+	sparkMesh.rotation.y = Math.PI / 2
+	sparkMesh.bakeCurrentTransformIntoVertices()
+
+	sparkMesh.lookAt(Vector3.UpReadOnly)
+
+	orbMesh.billboardMode = Mesh.BILLBOARDMODE_ALL
+	
+	return {
+		shootRailgun(robotRightGunPosition, pickedPoint) {
+			makeRayMesh(robotRightGunPosition, pickedPoint, sparkMesh, orbMesh, scene)
+		}
+	}
+}
