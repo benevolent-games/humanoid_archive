377940a25954ce6f4e82d13f6a930e8ddca35093
zenkyu <przemekgg2002@gmail.com>
2023-05-14 13:48:34 +0200

    add: rocket launcher

--- s/utils/rocket-launcher-vfx.ts
+++ s/utils/rocket-launcher-vfx.ts
@@ -1,6 +1,7 @@
 import {Scene} from "@babylonjs/core/scene.js"
 import {Ray} from "@babylonjs/core/Culling/ray.js"
 import {Mesh} from "@babylonjs/core/Meshes/mesh.js"
+import {Sprite} from "@babylonjs/core/Sprites/sprite.js"
 import {MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder.js"
 import {AbstractMesh} from "@babylonjs/core/Meshes/abstractMesh.js"
 import {PickingInfo} from "@babylonjs/core/Collisions/pickingInfo.js"
@@ -14,8 +15,11 @@ import {createBulletTexture} from "./create-bullet-texture.js"
 import {createBlastDotTexture} from "./create-blastdot-texture.js"
 import {createConeBlastTexture} from "./create-coneblast-texture.js"
 import {TransformNode} from "@babylonjs/core/Meshes/transformNode.js"
+import {SpriteManager} from "@babylonjs/core/Sprites/spriteManager.js"
 
-export class BlasterVFX{
+const sprite_url = "https://i.imgur.com/E5kbGTE.png"
+
+export class RocketLauncherVFX{
 name: string
 args: any
 scene: Scene
@@ -48,7 +52,7 @@ mesh: Mesh
 			blastCone.rotation.x = Math.PI*0.5
 			blastCone.bakeCurrentTransformIntoVertices()
 			this.blastConeMat = createConeBlastTexture('standard:blastCone', {res:512}, scene)
-			blastCone.material = this.blastConeMat    
+			blastCone.material = this.blastConeMat
 			this.sps.addShape(blastCone, args.cache)
 		
 			let bulletShape = MeshBuilder.CreateRibbon('bullet', {
@@ -80,32 +84,20 @@ mesh: Mesh
 			bulletShape.bakeCurrentTransformIntoVertices()
 
 			this.bulletMat = createBulletTexture('bullet:standard', {res:512}, scene)
-			bulletShape.material = this.bulletMat    
+			bulletShape.material = this.bulletMat
 			this.sps.addShape(bulletShape, args.cache)
 			bulletShape.dispose()
 
-			//ReuseBlastCone
-			this.sps.addShape(blastCone, args.cache)
-			blastCone.dispose()
-
 			let sparksMat = new StandardMaterial(this.name+':sparksMat', scene)
-			sparksMat.diffuseTexture = new Texture("https://www.babylonjs-playground.com/textures/flare.png", scene)
+			sparksMat.diffuseTexture = new Texture("", scene)
 			sparksMat.emissiveTexture = sparksMat.diffuseTexture
-			sparksMat.opacityTexture = new Texture("https://www.babylonjs-playground.com/textures/flare.png", scene)
+			sparksMat.opacityTexture = new Texture("", scene)
 			sparksMat.opacityTexture.getAlphaFromRGB = true
 
-			
-			
 			let sparkPlane = Mesh.CreatePlane("sparkPlane", 1, scene)
 			sparkPlane.material = sparksMat
 			this.sps.addShape(sparkPlane, args.cache)
 			sparkPlane.dispose()
-
-			let splashDotMat = createBlastDotTexture(this.name+':standardDot', {res:256}, scene)
-			let splashDotPlane = Mesh.CreatePlane("splashDotPlane", 1, scene)
-			splashDotPlane.material = splashDotMat
-			this.sps.addShape(splashDotPlane, args.cache)
-			splashDotPlane.dispose()
 
 			this.mesh = this.sps.buildMesh()
 			
@@ -128,35 +120,11 @@ mesh: Mesh
 			}
 
 			this.sps.updateParticle = (p: any)=>{
-					let aTime, xOff, yOff
 					switch(p.type){
 						case 0:
 							if(this.aimNode)
 								p.position = this.aimNode.getAbsolutePosition()
 							break;
-						case 1:
-									p.timer += this.delta
-									aTime = Math.min(p.timer/p.animationSpeed, 1)
-									xOff = Math.floor(aTime/p.animationStep)
-									if(xOff==p.lastFrame){
-											this.sps.recycleParticle(p)
-									}
-									yOff = Math.floor(xOff/p.pow2Count)
-									xOff -= (yOff*p.pow2Count)
-
-									xOff *= p.textureCellSize
-									yOff *= p.textureCellSize
-
-									p.uvs.x = xOff
-									p.uvs.y = yOff
-									p.uvs.z = xOff + p.textureCellSize
-									p.uvs.w = yOff + p.textureCellSize
-
-									p.scale.scaleInPlace(p.scaleUp)
-									if(p.scale.x>=p.scaleMax){
-											return this.sps.recycleParticle(p)
-									}
-							break
 							case 2 :
 									if(p.scale.z < p.scaleMax){
 											p.scale.z*=p.scaleUp
@@ -195,37 +163,6 @@ mesh: Mesh
 											}
 									}
 							break
-							case 3 :
-									p.timer += this.delta
-									aTime = Math.min(p.timer/p.animationSpeed, 1)
-									xOff = Math.floor(aTime/p.animationStep)
-									if(xOff==p.lastFrame){
-											this.sps.recycleParticle(p)
-									}
-									yOff = Math.floor(xOff/p.pow2Count)
-									xOff -= (yOff*p.pow2Count)
-
-									xOff *= p.textureCellSize
-									yOff *= p.textureCellSize
-
-									p.uvs.x = xOff
-									p.uvs.y = yOff
-									p.uvs.z = xOff + p.textureCellSize
-									p.uvs.w = yOff + p.textureCellSize
-
-									let pp = this.sps.particles[p.parent]
-									if(pp.distanceDelta <= 0){
-											return this.sps.recycleParticle(p)
-									}
-									p.scale.scaleInPlace(p.scaleUp*pp.distanceDelta)
-									if(p.scale.x>=p.scaleMax*pp.distanceDelta){
-											return this.sps.recycleParticle(p)
-									}
-									
-									p.position = pp.position.clone()
-									p.rotation = pp.rotation.clone()
-
-							break
 							case 4:
 									p.scale.scaleInPlace(p.scaleDown)
 									if(p.scale.x<=0.01){
@@ -234,24 +171,6 @@ mesh: Mesh
 									p.speed *= p.drag
 									p.position.addInPlace(p.direction.scale(p.speed))
 									p.direction.subtractInPlace(Vector3.Up().scale(p.drop))
-							break;
-							case 5:
-									p.timer += this.delta
-									aTime = Math.min(p.timer/p.animationSpeed, 1)
-									xOff = Math.floor(aTime/p.animationStep)
-									if(xOff==p.lastFrame){
-											this.sps.recycleParticle(p)
-									}
-									yOff = Math.floor(xOff/p.pow2Count)
-									xOff -= (yOff*p.pow2Count)
-
-									xOff *= p.textureCellSize
-									yOff *= p.textureCellSize
-
-									p.uvs.x = xOff
-									p.uvs.y = yOff
-									p.uvs.z = xOff + p.textureCellSize
-									p.uvs.w = yOff + p.textureCellSize
 							break;
 					}
 			}
@@ -284,21 +203,16 @@ mesh: Mesh
 	hit(p, pick: PickingInfo, rotation: any){
 			let normal = p.direction.scale(-1)
 			let point = pick.pickedPoint
+			let normalP = pick!.pickedMesh!.rotation.y
+
 			this.subEmit(4, {
 					parentId : p.idx,
 					position : point,
 					speed : p.speed,
 					direction: normal,
-					rotation
+					rotation,
+					normal: normalP
 			}, (Math.random()*3)+6)
-			this.subEmit(1, {
-					position:p.position
-			})
-			this.subEmit(5, {
-					position:p.position,
-					distance: p.distanceDelta,
-					direction: p.direction
-			})
 			this.sps.recycleParticle(p)
 	}
 
@@ -313,7 +227,7 @@ mesh: Mesh
 			}
 	}
 
-	fire(robotRightGun: AbstractMesh) {
+	fire(robotRightGun: AbstractMesh, normal: Vector3) {
 			const gunPosition = robotRightGun.getAbsolutePosition()
 			const gunForward = robotRightGun.forward
 			//MuzzleFlash
@@ -333,45 +247,6 @@ mesh: Mesh
 			p.uvs.z = p.textureCellSize
 			p.uvs.w = p.textureCellSize
 			p.animationSpeed = 200
-			p.lastFrame = p.pow2Count*p.pow2Count
-			p.animationStep = 1/(p.lastFrame+1)
-
-			p = this.sps.particles[this.getCurrentSpawn(0)]
-			p.type = 1
-			p.scale = new Vector3(0.1,0.1,0.1)
-			p.rotation = this.aimNode!.rotation.clone()
-			p.position =  new Vector3(gunPosition.x, gunPosition.y, gunPosition.z)
-			p.scaleUp = 1.1
-			p.scaleMax = 0.5
-			p.timer = 0
-			p.pow2Count = 3
-			p.textureCellSize = 1/p.pow2Count 
-			p.frameID = 0
-			p.uvs.x = 0.0
-			p.uvs.y = 0.0
-			p.uvs.z = p.textureCellSize
-			p.uvs.w = p.textureCellSize
-			p.animationSpeed = 200
-			p.lastFrame = p.pow2Count*p.pow2Count
-			p.animationStep = 1/(p.lastFrame+1)
-			
-		// 	//ConeBlast
-			p = this.sps.particles[this.getCurrentSpawn(1)]
-			p.type = 1
-			p.scale = new Vector3(0.2,0.2,0.2)
-			p.rotation = this.aimNode!.rotation.clone()
-			p.position = new Vector3(0,0,0)
-			p.scaleUp = 1.08
-			p.scaleMax = 0.8
-			p.timer = 0
-			p.pow2Count = 3
-			p.textureCellSize = 1/p.pow2Count 
-			p.frameID = 0
-			p.uvs.x = 0.0
-			p.uvs.y = 0.0
-			p.uvs.z = p.textureCellSize
-			p.uvs.w = p.textureCellSize
-			p.animationSpeed = 400
 			p.lastFrame = p.pow2Count*p.pow2Count
 			p.animationStep = 1/(p.lastFrame+1)
 
@@ -396,94 +271,16 @@ mesh: Mesh
 			p.subEmitTick = 0
 	}
 
-	subEmit(type: number, data: any, count = 1){
-			let p
+	subEmit(type: number, data: any, count = 3){
 			switch(type){
-					case 1:
-							p = this.sps.particles[this.getCurrentSpawn(0)]
-							p.type = 1
-							p.scale = new Vector3(0.5,0.5,0.5)
-							p.position = data.position.clone()
-							p.scaleUp = 1.1
-							p.scaleMax = 2.
-							p.timer = 0
-							p.pow2Count = 3
-							p.textureCellSize = 1/p.pow2Count 
-							p.frameID = 0
-							p.uvs.x = 0.0
-							p.uvs.y = 0.0
-							p.uvs.z = p.textureCellSize
-							p.uvs.w = p.textureCellSize
-							p.animationSpeed = 400
-							p.lastFrame = p.pow2Count*p.pow2Count
-							p.animationStep = 1/(p.lastFrame+1)
-					break;
-					//Frontal Cone
-					case 3:
-							p = this.sps.particles[this.getCurrentSpawn(3)]
-							p.type = 3
-							p.scale = new Vector3(0.3,0.3,0.3)
-							p.position = data.position
-							p.scaleUp = 1.12
-							p.scaleMax = 0.8
-							p.timer = 0
-							p.pow2Count = 3
-							p.textureCellSize = 1/p.pow2Count
-							p.frameID = 0
-							p.uvs.x = 0.0
-							p.uvs.y = 0.0
-							p.uvs.z = p.textureCellSize
-							p.uvs.w = p.textureCellSize
-							p.animationSpeed = 50
-							p.lastFrame = p.pow2Count*p.pow2Count
-							p.animationStep = 1/(p.lastFrame+1)
-							p.parent = data.parentId
-					break;
 					case 4:
-							for(let i=0; i<count; i++){
-									p = this.sps.particles[this.getCurrentSpawn(4)]
-									p.type = 4
-									p.scale = (new Vector3(0.5,0.5,0.5)).scale(((Math.random()*0.5)+0.5))
-									p.position = data.position.clone()
-									p.scaleDown = 0.98-(Math.random()*0.2)
-									p.drop = (Math.random()*0.05)+0.02
-									p.drag = 0.86
-									p.speed = data.speed * ((Math.random()*0.25)+0.2)
-									p.direction = data.direction.clone()
-									let mr = Math.random()*10.
-									p.direction.x += (Math.sin(this.time*mr))
-									p.direction.y += (Math.cos(this.time*mr))
-									p.direction.z += (Math.sin(this.time*mr))
-									p.direction = p.direction.normalize()
-									p.rotationQuaternion = data.rotation
-							}
-					break;
-					case 5:
-									p = this.sps.particles[this.getCurrentSpawn(5)]
-									p.type = 5
-									p.scale = new Vector3(0.3, 0.3, 0.3).scale(data.distance)
-									p.position = data.position.clone()
-									p.direction = data.direction
-									p.timer = 0
-									p.pow2Count = 3
-									p.textureCellSize = 1/p.pow2Count 
-									p.frameID = 0
-									p.uvs.x = 0.0
-									p.uvs.y = 0.0
-									p.uvs.z = p.textureCellSize
-									p.uvs.w = p.textureCellSize
-									p.animationSpeed = 3000
-									p.lastFrame = p.pow2Count*p.pow2Count
-									p.animationStep = 1 / (p.lastFrame + 1)
-									const yawAngle = -Math.atan2(p.direction.z, p.direction.x) + Math.PI / 2
-									const pitchAngle = Math.atan2(p.direction.y, Math.sqrt(p.direction.x * p.direction.x + p.direction.z * p.direction.z))
-									const rollAngle = Math.atan2(p.direction.y, p.direction.x)
-									let matrix = Matrix.RotationYawPitchRoll(
-										yawAngle,
-										pitchAngle,
-										0
-									)
-									p.rotationQuaternion = Quaternion.FromRotationMatrix(matrix)
+							const position = data.position.clone()
+							const spriteManager = new SpriteManager("explosionManager", sprite_url, 16, 48, this.scene);
+							const explosionSprite = new Sprite("explosion", spriteManager)
+							explosionSprite.position = position
+							explosionSprite.size = 3
+							explosionSprite.angle = data.normal * 180 / Math.PI
+							explosionSprite.playAnimation(0, 8, false, 50)
 					break;
 			}
 	}
@@ -499,11 +296,11 @@ mesh: Mesh
 			}
 			return s
 	}
-	shootBlaster(
-		blast: BlasterVFX,
+	shootRocketLauncher(
+		blast: RocketLauncherVFX,
 		scene: Scene,
 		robotRightGun: AbstractMesh,
-		activeCapsule: Mesh) {
+		normal: Vector3) {
 		let timer = 300
 		let last = Date.now()
 		let time = 0
@@ -518,7 +315,7 @@ mesh: Mesh
 				last = n
 			}
 		})
-		blast.fire(robotRightGun)
+		blast.fire(robotRightGun, normal)
 		e!.unregisterOnNextCall = true
 	}
 }
